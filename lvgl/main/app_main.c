1132f960cc8cbab31538e31f97f39a432df0da6b
Hyouck Kim <hawk.kim1974@gmail.com>
2026-03-12 01:49:35 +0900

    added realtime cpu usage tracking

--- lvgl/main/app_main.c
+++ lvgl/main/app_main.c
@@ -35,6 +35,7 @@ static uint32_t t = 0;  // time counter
 static QueueHandle_t gpio_evt_queue = NULL;
 
 size_t total_sram_in_KB = 0;
+volatile uint32_t cpu_usage = 0;
 
 static void
 change_led(void)
@@ -107,6 +108,107 @@ configure_gpio_input(void)
   gpio_isr_handler_add(GPIO_INPUT_IO_9, gpio_isr_handler, (void*) GPIO_INPUT_IO_9);
 }
 
+static esp_err_t
+print_real_time_stats(TickType_t xTicksToWait, uint32_t* usage)
+{
+  TaskStatus_t *start_array = NULL, *end_array = NULL;
+  UBaseType_t start_array_size, end_array_size;
+  configRUN_TIME_COUNTER_TYPE start_run_time, end_run_time;
+  esp_err_t ret;
+
+  //Allocate array to store current task states
+  #define ARRAY_SIZE_OFFSET   5
+
+  *usage = 0;
+
+  start_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
+  start_array = malloc(sizeof(TaskStatus_t) * start_array_size);
+  if (start_array == NULL) {
+    ret = ESP_ERR_NO_MEM;
+    goto exit;
+  }
+  //Get current task states
+  start_array_size = uxTaskGetSystemState(start_array, start_array_size, &start_run_time);
+  if (start_array_size == 0) {
+    ret = ESP_ERR_INVALID_SIZE;
+    goto exit;
+  }
+
+  vTaskDelay(xTicksToWait);
+
+  //Allocate array to store tasks states post delay
+  end_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
+  end_array = malloc(sizeof(TaskStatus_t) * end_array_size);
+  if (end_array == NULL) {
+    ret = ESP_ERR_NO_MEM;
+    goto exit;
+  }
+  //Get post delay task states
+  end_array_size = uxTaskGetSystemState(end_array, end_array_size, &end_run_time);
+  if (end_array_size == 0) {
+    ret = ESP_ERR_INVALID_SIZE;
+    goto exit;
+  }
+
+  //Calculate total_elapsed_time in units of run time stats clock period.
+  uint32_t total_elapsed_time = (end_run_time - start_run_time);
+  if (total_elapsed_time == 0) {
+    ret = ESP_ERR_INVALID_STATE;
+    goto exit;
+  }
+
+  //Match each task in start_array to those in the end_array
+  for (int i = 0; i < start_array_size; i++) {
+    int k = -1;
+    for (int j = 0; j < end_array_size; j++) {
+      if (start_array[i].xHandle == end_array[j].xHandle) {
+        k = j;
+        //Mark that task have been matched by overwriting their handles
+        start_array[i].xHandle = NULL;
+        end_array[j].xHandle = NULL;
+        break;
+      }
+    }
+    //Check if matching task found
+    if (k >= 0) {
+      uint32_t task_elapsed_time = end_array[k].ulRunTimeCounter - start_array[i].ulRunTimeCounter;
+      uint32_t percentage_time = (task_elapsed_time * 100UL) / (total_elapsed_time * CONFIG_FREERTOS_NUMBER_OF_CORES);
+
+      // printf("| %s | %"PRIu32" | %"PRIu32"%%\n", start_array[i].pcTaskName, task_elapsed_time, percentage_time);
+      //
+      if(strcmp(start_array[i].pcTaskName, "IDLE") == 0)
+      {
+        *usage = (uint32_t)(100 - percentage_time);
+        break;
+      }
+    }
+  }
+
+  ret = ESP_OK;
+
+exit:    //Common return path
+  free(start_array);
+  free(end_array);
+  return ret;
+}
+
+static void stats_task(void *arg)
+{
+  uint32_t usage;
+
+  //Print real time stats periodically
+  while (1) {
+    if (print_real_time_stats(pdMS_TO_TICKS(1000), &usage) == ESP_OK) {
+      // printf("Real time stats obtained\n");
+      cpu_usage = usage;
+    } else {
+      // printf("Error getting real time stats\n");
+    }
+    // vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
+
 void
 app_main(void)
 {
@@ -128,6 +230,7 @@ app_main(void)
   my_lvgl_app_init();
 
   ESP_LOGI(TAG, "Free heap after init %dKB", esp_get_free_internal_heap_size() / 1024);
+  xTaskCreatePinnedToCore(stats_task, "stats", 4096, NULL, 3, NULL, tskNO_AFFINITY);
 
   while (1)
   {
